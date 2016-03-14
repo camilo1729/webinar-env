59b7ef61e09fa34e974a3e15a40554a082441a78
Cristian Ruiz <cristian.ruiz@inria.fr>
2016-03-14 14:02:08 +0100

    Fixed logos urls

--- download_theme.sh
+++ download_theme.sh
@@ -3,7 +3,7 @@
  mkdir theme
  wget https://raw.githubusercontent.com/camilo1729/latex-tools/master/beamer_theme/beamerthemeCristian.sty
  mv beamerthemeCristian.sty  theme/
- wget https://github.com/camilo1729/latex-tools/blob/master/logos/grid5000.png
- wget https://github.com/camilo1729/latex-tools/blob/master/logos/inr_logo_sans_sign_coul.png
+ wget https://github.com/camilo1729/latex-tools/raw/master/logos/grid5000.png
+ wget https://github.com/camilo1729/latex-tools/raw/master/logos/inr_logo_sans_sign_coul.png
  mkdir logos
  mv *.png logos
