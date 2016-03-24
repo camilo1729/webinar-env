aa6a6291df89efa8b2a05a35eb3156732e5befca
Arnaud Legrand <arnaud.legrand@imag.fr>
2016-03-24 23:26:01 +0100

    Get rid of many completely useless latex dependencies and replace by my own style. ;) Advantages: - Red is nicer than dull blueish gray ;) - References will be highlighted - Alert in * * is a dark red do not kill your eyes - Does not depend on fontspec that asked about something else than pdflatex!

--- download_theme.sh
+++ download_theme.sh
@@ -2,6 +2,8 @@
 
  mkdir theme
  wget https://raw.githubusercontent.com/camilo1729/latex-tools/master/beamer_theme/beamerthemeCristian.sty
+ wget https://raw.githubusercontent.com/alegrand/SMPE/master/lectures/org-babel-document-preembule.tex
+ wget https://raw.githubusercontent.com/alegrand/SMPE/master/lectures/org-babel-style-preembule.tex
  mv beamerthemeCristian.sty  theme/
  wget https://github.com/camilo1729/latex-tools/raw/master/logos/grid5000.png
  wget https://github.com/camilo1729/latex-tools/raw/master/logos/inr_logo_sans_sign_coul.png
